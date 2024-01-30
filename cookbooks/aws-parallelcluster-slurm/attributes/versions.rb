a9c38aea8ea954702507228844d9962b1d0f9fc0
Hanwen <hanwenli@amazon.com>
2024-01-30 06:31:25 -0800

    Revert "Upgrade Slurm to version 23.11.0"
    This reverts commit 2442f50da09072e89711debce3e79d0209b00a06.

--- cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
@@ -1,9 +1,5 @@
 # Slurm
-default['cluster']['slurm']['version'] = '23-11-0-1'
-default['cluster']['slurm']['commit'] = ''
-default['cluster']['slurm']['branch'] = ''
-default['cluster']['slurm']['sha256'] = '3780773a80b73ea2edb4353318b4220188f4eda92c31ab3a2bdd3a4fdec76be9'
-default['cluster']['slurm']['base_url'] = "https://github.com/SchedMD/slurm/archive"
+default['cluster']['slurm']['version'] = '23-02-6-1'
 # Munge
 default['cluster']['munge']['munge_version'] = '0.5.15'
 default['cluster']['munge']['sha256'] = '51b2c81d1a7ec2ab5d486fa51b50c7e79eb1810ca6687b6ed65f3601abc55614'
