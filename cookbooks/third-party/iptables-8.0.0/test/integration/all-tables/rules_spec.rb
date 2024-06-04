0b6230e009f659615f98d4227cdf8491f062dd48
Hanwen <hanwenli@amazon.com>
2024-06-04 08:36:12 -0700

    Remove iptables 8.0.0 third-party cookbook
    It is no longer used by our code
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/third-party/iptables-8.0.0/test/integration/all-tables/rules_spec.rb
+++
@@ -1,7 +0,0 @@
-describe command('/sbin/iptables-save') do
-  its(:stdout) { should match /\*filter/ }
-  its(:stdout) { should match /\*mangle/ }
-  its(:stdout) { should match /\*nat/ }
-  its(:stdout) { should match /\*raw/ }
-  its(:stdout) { should match /\*security/ }
-end
