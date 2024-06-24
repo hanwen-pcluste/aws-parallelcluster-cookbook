04a86fbd1607752a5bb76ce27f687d6f5609eb0b
Hanwen <hanwenli@amazon.com>
2024-06-24 13:35:06 -0700

    further improvement for hpc
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/aws-parallelcluster-environment/files/amazonlinux-2023/network_interfaces/configure_nw_interface.sh
+++ cookbooks/aws-parallelcluster-environment/files/amazonlinux-2023/network_interfaces/configure_nw_interface.sh
@@ -5,6 +5,7 @@ set -ex
 if
   [ -z "${DEVICE_NAME}" ] ||          # name of the device
   [ -z "${DEVICE_NUMBER}" ]         # number of the device
+  [ -z "${DEVICE_IP_ADDRESS}" ]         # ip of the device
 then
   echo 'One or more environment variables missing'
   exit 1
@@ -13,10 +14,17 @@ echo "Device name: ${DEVICE_NAME}, Device number: ${DEVICE_NUMBER}"
 
 [ "$DEVICE_NUMBER" -eq "0" ] && exit 0
 
-echo "helllllllo"
 sudo cp -nr /run/systemd/network /etc/systemd
-echo "helllllllo2"
 cd /etc/systemd/network
+ROUTE_TABLE=200${DEVICE_NUMBER}
 files_list=$(find . -type f | grep "$DEVICE_NAME")
-sudo sed -i "s/Table=.*/Table=200${DEVICE_NUMBER}/g" $files_list
-echo "hellllllloend"
+sudo sed -i "s/Table=.*/Table=${ROUTE_TABLE}/g" $files_list
+
+if ! grep "RoutingPolicyRule" $files_list; then
+/bin/cat <<EOF >rule-${DEVICE_NAME}
+[RoutingPolicyRule]
+From=${DEVICE_IP_ADDRESS}
+Priority=10001
+Table=${ROUTE_TABLE}
+EOF
+fi
